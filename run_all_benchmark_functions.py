015f3a1f29859c754d50d89a7de03787932165a4
vutngn <vutngn@amazon.com>
2023-02-22 09:57:17 +1030

    update

--- run_all_benchmark_functions.py
+++ run_all_benchmark_functions.py
@@ -27,72 +27,59 @@ warnings.filterwarnings("ignore")
 
 counter = 0
 
+# Select a list of benchmark functions to run the comparison
+function_list=[]
+function_list.append(functions.branin())
+function_list.append(functions.hartman_3d())
+function_list.append(functions.ackley(input_dim=5))
+function_list.append(functions.alpine1(input_dim=5))
+function_list.append(functions.hartman_6d())
+function_list.append(functions.gSobol(a=np.array([1,1,1,1,1])))
 
-myfunction_list=[]
-
-#myfunction_list.append(functions.sincos())
-myfunction_list.append(functions.branin())
-#myfunction_list.append(functions.hartman_3d())
-#myfunction_list.append(functions.ackley(input_dim=5))
-#myfunction_list.append(functions.alpine1(input_dim=5))
-#myfunction_list.append(functions.hartman_6d())
-#myfunction_list.append(functions.gSobol(a=np.array([1,1,1,1,1])))
-#myfunction_list.append(functions.gSobol(a=np.array([1,1,1,1,1,1,1,1,1,1])))
-
-
+# Select a list of baselines to compare
 acq_type_list=[]
-
 
 temp={}
 temp['name']='erm' # expected regret minimization
-temp['IsTGP']=0 # recommended to use tgp for ERM
+temp['IsTGP']=1 # recommended to use tgp for ERM
 acq_type_list.append(temp)
 
 temp={}
 temp['name']='cbm' # confidence bound minimization
 temp['IsTGP']=1 # recommended to use tgp for CBM
-#acq_type_list.append(temp)
-
+acq_type_list.append(temp)
 
 #temp={}
 #temp['name']='kov_mes' # MES+f*
 #temp['IsTGP']=0 # we can try 'tgp'
 #acq_type_list.append(temp)
 
-
-
-temp={}
-temp['name']='kov_ei' # this is EI + f*
-temp['IsTGP']=0 # we can try 'tgp' by setting it =1
+# temp={}
+# temp['name']='kov_ei' # this is EI + f*
+# temp['IsTGP']=0 # we can try 'tgp' by setting it =1
 #acq_type_list.append(temp)
-
-
 
 temp={}
 temp['name']='ucb' # vanilla UCB
 temp['IsTGP']=0 # we can try 'tgp' by setting it =1
-#acq_type_list.append(temp)
-
-
+acq_type_list.append(temp)
 
 temp={}
 temp['name']='ei' # vanilla EI
 temp['IsTGP']=0 # we can try 'tgp' by setting it =1
 acq_type_list.append(temp)
 
-
-
 temp={}
 temp['name']='random' # random
 temp['IsTGP']=0 # we can try 'tgp' by setting it =1
-acq_type_list.append(temp)
+#acq_type_list.append(temp)
 
 fig=plt.figure()
 
-color_list=['r','b','k','m','c','g','o']
-marker_list=['s','x','o','v','^','>','<']
+color_list=['r','b','k','m','c','g']
+marker_list=['s','x','o','v','^','>']
 
-for idx, (myfunction,acq_type,) in enumerate(itertools.product(myfunction_list,acq_type_list)):
+for idx, (myfunction,acq_type,) in enumerate(itertools.product(function_list,acq_type_list)):
 
     print("=====================func:",myfunction.name)
     print("==================acquisition type",acq_type)
@@ -104,13 +91,11 @@ for idx, (myfunction,acq_type,) in enumerate(itertools.product(myfunction_list,a
     
     ybest=[0]*nRepeat
     MyTime=[0]*nRepeat
-    MyOptTime=[0]*nRepeat
+    OptTime=[0]*nRepeat
     marker=[0]*nRepeat
 
     bo=[0]*nRepeat
-   
-    [0]*nRepeat
-    
+       
     for ii in range(nRepeat):
         
         if 'kov' in acq_name or acq_name == 'erm' or acq_name == 'cbm':
@@ -123,24 +108,20 @@ for idx, (myfunction,acq_type,) in enumerate(itertools.product(myfunction_list,a
             ybest[ii],MyTime[ii]=utilities.run_experiment(bo[ii],
                  n_init=13*myfunction.input_dim,NN=0,runid=ii)   
         else:
-            ybest[ii],MyTime[ii]=utilities.run_experiment(bo[ii],n_init=3*myfunction.input_dim,\
+            ybest[ii],MyTime[ii] = utilities.run_experiment(bo[ii],n_init=3*myfunction.input_dim,\
              NN=10*myfunction.input_dim,runid=ii)   
                                        
-        MyOptTime[ii]=bo[ii].time_opt
-        print("ii={} BFV={:.3f}".format(ii,myfunction.ismax*np.max(ybest[ii])))                                              
+        OptTime[ii] = bo[ii].time_opt
+        print("ii={} Best Found Value={:.3f}".format(ii,myfunction.ismax*np.max(ybest[ii])))                                              
         
-
-    Score={}
-    Score["ybest"]=ybest
-    Score["MyTime"]=MyTime
-    Score["MyOptTime"]=MyOptTime
+    # record the result and save to pickle files================================
+    # Score={}
+    # Score["ybest"]=ybest
+    # Score["MyTime"]=MyTime
+    # Score["OptTime"]=OptTime
+    # utilities.print_result_sequential(bo,myfunction,Score,acq_type) 
     
-    utilities.print_result_sequential(bo,myfunction,Score,acq_type) 
-    
-    
-    ## plot the result
-    # process the result
-    
+    ## plot the result    
     y_best_sofar=[0]*len(bo)
     for uu,mybo in enumerate(bo):
         y_best_sofar[uu]=[ (myfunction.fstar - np.max(mybo.Y_ori[:ii+1]) ) for ii in range(len(mybo.Y_ori))]
@@ -148,11 +129,9 @@ for idx, (myfunction,acq_type,) in enumerate(itertools.product(myfunction_list,a
         
     y_best_sofar=np.asarray(y_best_sofar)
     
-    myxaxis=range(y_best_sofar.shape[1])
-    
-    plt.errorbar(myxaxis,np.mean(y_best_sofar,axis=0), np.std(y_best_sofar,axis=0)/np.sqrt(nRepeat),
-                 label=acq_type['name'],color=color_list[idx],marker=marker_list[idx])
-    
+    xaxis=range(y_best_sofar.shape[1])
+    # plt.errorbar(xaxis,np.mean(y_best_sofar,axis=0), np.std(y_best_sofar,axis=0)/np.sqrt(nRepeat),
+    #              label=acq_type['name'],color=color_list[idx],marker=marker_list[idx])
     
     
 plt.ylabel("Simple Regret",fontsize=14)
